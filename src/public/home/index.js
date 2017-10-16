0b55c0c66a282eb0087e212194cf89eff4192193
Ugur Kodak <ugurkodak@gmail.com>
2017-10-16 19:34:53 -0400

    Init glowing particles

--- src/public/home/index.js
+++ src/public/home/index.js
@@ -20,15 +20,18 @@ if (Detector.webgl) {
     var mouseX = 0;
     var mouseY = 0;
     var mouseDown = false;
-    document.onmousemove = function(e) {
-	mouseX = e.clientX;
-	mouseY = e.clientY;
+    var clickPosition = new THREE.Vector2();
+    document.onmousemove = function (e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
     }
-    document.onmousedown = function(e) {
-	mouseDown = true;
+    document.onmousedown = function (e) {
+        mouseDown = true;
+        clickPosition.setX(mouseX);
+        clickPosition.setY(mouseY);
     }
-    document.onmouseup = function(e) {
-	mouseDown = false;
+    document.onmouseup = function (e) {
+        mouseDown = false;
     }
 
     //Setup scene
@@ -39,6 +42,14 @@ if (Detector.webgl) {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    //Window resize
+    window.addEventListener('resize', onWindowResize, false);
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
     //Add stars
     var starsGeometry = new THREE.Geometry();
     for (var i = 0; i < 10000; i++) {
@@ -48,36 +59,31 @@ if (Detector.webgl) {
         star.z = THREE.Math.randFloatSpread(2000);
         starsGeometry.vertices.push(star);
     }
-    var starsMaterial = new THREE.PointsMaterial({ 
-        color: 0xffffff 
+    var starsMaterial = new THREE.PointsMaterial({
+        color: 0xffffff
     });
     var starField = new THREE.Points(starsGeometry, starsMaterial);
     starField.position.z = -1000;
-    scene.add( starField );
+    scene.add(starField);
 
-    //Sphere
-    var sphereSpeed = 0;
-    var geometry = new THREE.SphereGeometry(0.5, 32, 32);
-    var material = new THREE.MeshStandardMaterial();
-    var sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
+    //Glowing objects
+    var glowingObject = new THREE.Object3D();
+    var glowTexture = new THREE.TextureLoader().load("home/glow.png");
+    var glowMaterial = new THREE.SpriteMaterial({
+        map: glowTexture, 
+        color: 0xff00ff
+    });
+    for (var i = 0; i < 100; i++) {
+        var glowParticle = new THREE.Sprite(glowMaterial);
+        //glowParticle.position.x = THREE.Math.randFloatSpread(1);
+        //glowParticle.position.y = THREE.Math.randFloatSpread(1);
+        //glowParticle.position.z = THREE.Math.randFloatSpread(1);
+        //glowParticle.scale.set(0.2, 0.2, 1)
+        glowingObject.add(glowParticle);
+    }    
+    glowingObject.position.z = -100;
+    scene.add(glowingObject);
 
-    var spotLight = new THREE.SpotLight(0xff00ff, 1);
-    spotLight.position.set(0, 0, 100);
-    scene.add(spotLight);
-
-    var ambientLight = new THREE.AmbientLight(0xff0000, 0.5);
-    spotLight.position.set(0, 0, 2);
-    scene.add(ambientLight);
-
-    //Window resize
-    window.addEventListener( 'resize', onWindowResize, false );
-    function onWindowResize(){
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-    }
-    
     animate();
 } else {
     //WebGL not available
@@ -86,12 +92,34 @@ if (Detector.webgl) {
 }
 
 function animate() {
+    //Loop and render
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    
+
+    //Animate stars
     starField.rotation.x += 0.0005;
     starField.rotation.y += 0.0005;
-    //spotLight.position.set(2000 * Math.cos(movementIterator), 2000 * Math.sin(movementIterator), 5);
-    sphere.position.set(0.8 * Math.cos(sphereSpeed), 0.8 * Math.sin(sphereSpeed), 0);
-    sphereSpeed += 0.01;
+
+    //Animate glowing object
+    for (var i = 0; i < glowingObject.children.length; i++) {
+        particlePosition = glowingObject.children[i].position;
+       // if (mouseDown) {
+            //var signedRandomMultiplierX = ;
+            var signedRandomMultiplierY = Math.random() < 0.5 ? -1 : 1;
+            var signedRandomMultiplierX = Math.random() < 0.5 ? -1 : 1;
+            particlePosition.x += (Math.random() < 0.5 ? -1 : 1) * 0.01;
+            particlePosition.y += (Math.random() < 0.5 ? -1 : 1) * 0.01;
+            particlePosition.z += (Math.random() < 0.5 ? -1 : 1) * 0.01;
+            //THREE.Math.clamp(glowingObject.children[i].position.x, 0, 0.5);
+            //console.log(particlePosition);            
+            
+            
+            //glowingObject.children[i].position.x = THREE.Math.clamp(mouseX - glowingObject.children[i].position.x);
+            //glowingObject.children[i].position.y = THREE.Math.clamp(mouseY - glowingObject.children[i].position.x);
+            //glowingObject.children[i].position.z += signedRandom * Math.random * 0.001;            
+       // }
+    }
+    if (glowingObject.position.z < 0)
+    glowingObject.position.z += Math.abs(glowingObject.position.z) * 0.01;
+    //glowingObject.rotation.x += 0.01;
 }
